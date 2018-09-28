6580807f68678cdcc8e06df2dd23ec415f1a2016
ArseniiT <arseniit@gmail.com>
2018-09-28 14:51:15 -0400

    List of availables rooms created on the web page

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular2';
+export class AppComponent implements OnInit{
+
+  public submitted: boolean;
+  roomsearch : FormGroup;
+  rooms : Room[];
+
+  ngOnInit() {
+    this.roomsearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+
+    this.rooms = ROOMS;
+  }
+
+  onSubmit({value, valid}: {value: Roomsearch, valid: boolean}) {
+    console.log(value);
+  }
 }
+
+export interface Roomsearch {
+  checkin: string;
+  checkout: string;
+}
+
+export interface Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string;
+}
+
+var ROOMS: Room[] = [
+  {
+    id: "123546",
+    roomNumber: "409",
+    price: "200",
+    links: ""
+  },
+  {
+    id: "123546",
+    roomNumber: "410",
+    price: "150",
+    links: ""
+  },
+  {
+    id: "123544",
+    roomNumber: "411",
+    price: "205",
+    links: ""
+  }
+];
